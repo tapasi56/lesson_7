9e66f99ba52379ada0d17f90d74fd760e75ce013
Tapasi <33500374+tapasi56@users.noreply.github.com>
2025-09-02 18:32:32 +0100

    Add files via upload

--- cw.py
+++ cw.py
@@ -20,5 +20,62 @@ def create_satellite():
     for count in range(0,number_of_satellite):
         satellite = Actor("satellite")
         satellite.pos = randint(40,WIDTH - 40), randint(40, HEIGHT - 40)
-        satellite.append(satellite)
-    start_time = time{}
+        satellites.append(satellite)
+    start_time = time()
+
+
+def draw():
+    global total_time
+
+    screen.blit("background", (0,0))
+    number = 1
+
+    for satellite in satellites:
+        screen.draw.text(str(number), (satellite.pos[0], satellite.pos[1] + 20))
+        satellite.draw()
+        number = number + 1
+
+    for line in lines:
+        screen.draw.text(line[0], line[1], (255,254,253))
+
+    if next_satellite < number_of_satellite:
+        total_time = time() - start_time
+        screen.draw.text(str(round(total_time)), (10,10),fontsize = 30)
+
+    else:
+        screen.draw.text(str(round(total_time)), (10,10),fontsize = 30)
+
+    
+def update():
+        pass
+
+def on_mouse_down(pos):
+     
+     global next_satellite, lines
+     
+     if next_satellite < number_of_satellite:
+        if satellites[next_satellite].collidepoint(pos):
+            if next_satellite:
+                lines.append((satellites[next_satellite - 1].pos, satellites[next_satellite].pos))
+                next_satellite = next_satellite + 1
+
+        else:
+            lines = []
+            next_satellite = 0
+
+create_satellite()
+
+pgzrun.go()
+
+
+
+
+
+
+
+
+
+
+
+
+    
